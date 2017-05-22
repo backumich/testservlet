47128e8e4a3d7eb40b9c6bcd071e4bae502ee402
backumich <backumich@gmail.com>
2017-05-22 23:41:04 +0300

    Initial commit

---
+++ src/main/java/ua/com/vertex/OtherServlet.java
@@ -0,0 +1,31 @@
+package ua.com.vertex;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class OtherServlet extends HttpServlet {
+
+    String HTTP_EMAIL_RESPONSE = "<html>\n" +
+            "<head>\n" +
+            "    <title>Hello Servlet</title>\n" +
+            "</head>\n" +
+            "<body><h1>Your email was saved: %s</h1>" +
+            "<h2>Other</h2></body>\n" +
+            "</html>";
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        String email = req.getParameter("email");
+
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        writer.format(HTTP_EMAIL_RESPONSE, email);
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title>Test Servlet</title>
+</head>
+<body>
+<form method="POST" action="/test-servlet/">
+    Enter your email :
+    <input type="text" name="email">
+    <input type="submit" value="OK">
+</form>
+</body>
+</html>
